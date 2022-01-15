e8325c268f1f6f460c829c9e7dc126989fd9e9d5
Justen Di Ruscio <justend29@gmail.com>
2022-01-15 16:11:23 -0400

    remove tests from tests folder

--- planet.test.py
+++ planet.test.py
@@ -2,29 +2,6 @@ from planet import Planet
 import datetime
 import math
 
-# import unittest
-
-
-# class TestStringMethods(unittest.TestCase):
-#     def test_upper(self):
-#         self.assertEqual("foo".upper(), "FOO")
-
-#     def test_isupper(self):
-#         self.assertTrue("FOO".isupper())
-#         self.assertFalse("Foo".isupper())
-
-#     def test_split(self):
-#         s = "hello world"
-#         self.assertEqual(s.split(), ["hello", "world"])
-#         # check that s.split fails when the separator is not a string
-#         with self.assertRaises(TypeError):
-#             s.split(2)
-
-
-# if __name__ == "__main__":
-#     unittest.main()
-
-
 earth = Planet("paul", radius_sun=10)
 mercury = Planet("paul", 5, orbital_period=datetime.timedelta(days=10))
 pluto = Planet("paul", 50, orbital_period=datetime.timedelta(days=100))
