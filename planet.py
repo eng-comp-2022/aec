057e0f9c85565db426cb69d900ab27e3f6d8c2b6
pwalsh2 <paul.N.walsh@outlook.ca>
2022-01-15 14:44:15 -0400

    add some comments

--- planet.py
+++ planet.py
@@ -1,13 +1,17 @@
 import math
 from datetime import datetime, timedelta, timezone
 
-
+# Class Planet 
+'''
+All angle calculations were done with repect to earth becuase that was given
+as a reference point for the periods given.
+'''
 class Planet:
     def __init__(
         self,
         name,
         radius_sun,
-        orbital_period: timedelta = timedelta(),
+        orbital_period: timedelta = timedelta(),  # orbital adius is taken as a timedelta type
         last_op_earth: datetime = datetime.now(timezone.utc),
     ):
         self.name = name
@@ -15,12 +19,14 @@ class Planet:
         self.orbital_period = orbital_period
         self.last_op_earth = last_op_earth
 
-    # returns angle in radians of planet with respect to earth
+    # returns angle in radians of planet with respect to "earth"
     def location_angle_earth(self, total_orbiting_duration: timedelta):
+        
         current_orbit_duration = total_orbiting_duration % self.orbital_period
         percent_complete = current_orbit_duration / self.orbital_period
         return 2 * math.pi * percent_complete
 
+    # returns a datetime of the next opposition between the two planets
     def next_opposition_date(self, calculation_time=datetime.now(timezone.utc)):
         total_orbiting_duration = calculation_time - self.last_op_earth
         current_orbit_duration = total_orbiting_duration % self.orbital_period
