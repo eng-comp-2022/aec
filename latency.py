ae08f9bb95306bae41cc727440bd36c7f14474d9
pwalsh2 <paul.N.walsh@outlook.ca>
2022-01-15 14:31:08 -0400

    added some commenting

--- latency.py
+++ latency.py
@@ -4,7 +4,9 @@ import math
 from planet import Planet
 from scientific import light_speed, kin_duration
 
-
+# Computes the latncy between the two planets that are passed 
+# in as parameters. The parameter calculation_time is used to provide a relative 
+# time value to compute the distance betweent the two planets.
 def communication_latency(planet1: Planet, planet2: Planet, calculation_time: datetime):
     angle1 = planet1.location_angle_earth(calculation_time - planet1.last_op_earth)
     angle2 = planet2.location_angle_earth(calculation_time - planet2.last_op_earth)
--- moritorium.py
+++ moritorium.py
@@ -1,7 +1,8 @@
 import math
-
+# moratoriam tolerance is 7 degrees
 _moritorium_tolerance = 7 * math.pi / 180
 
+# calculate whether the 2 panets are in moratoriam.
 # provide angle in radians
 def is_moratorium(planet1_angle, planet2_angle):
     angle_diff = math.fabs(planet1_angle - planet2_angle)
