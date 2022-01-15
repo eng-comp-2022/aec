403bcc69d48d2b0e8e30c2a6bc73772e9a565b49
Justen Di Ruscio <justend29@gmail.com>
2022-01-15 14:10:44 -0400

    correct next opp date for any planets

--- planet.py
+++ planet.py
@@ -1,5 +1,6 @@
 import math
 from datetime import datetime, timedelta, timezone
+from scientific import angular_velocity
 
 
 class Planet:
@@ -21,10 +22,37 @@ class Planet:
         percent_complete = current_orbit_duration / self.orbital_period
         return 2 * math.pi * percent_complete
 
-    def next_opposition_date(self, calculation_time=datetime.now(timezone.utc)):
-        total_orbiting_duration = calculation_time - self.last_op_earth
-        current_orbit_duration = total_orbiting_duration % self.orbital_period
-        return calculation_time + self.orbital_period - current_orbit_duration
+    def next_opposition_date(self, other, calculation_time=datetime.now(timezone.utc)):
+        planet1_w = angular_velocity(self)
+        planet2_w = angular_velocity(other)
+        orbit_w = abs(planet1_w - planet2_w)
+
+        # faster planet moves at relative angular velocity wrt base planet
+        base_planet = self
+        orbiting_planet = other
+        if planet1_w < planet2_w:
+            base_planet = other
+            orbiting_planet = self
+
+        orbit_planet_angle = orbiting_planet.location_angle_earth(
+            calculation_time - orbiting_planet.last_op_earth
+        )
+        base_planet_angle = base_planet.location_angle_earth(
+            calculation_time - base_planet.last_op_earth
+        )
+
+        # remaining angle in orbit before opposition
+        diff_angle = math.abs(orbit_planet_angle - base_planet_angle)
+        angular_displacement = diff_angle
+        if orbit_planet_angle > base_planet_angle:
+            angular_displacement = 2 * math.pi - diff_angle
+
+        # convert remaining angle to duration
+        travel_duration_secs = angular_displacement / orbit_w
+        travel_duration = timedelta(seconds=travel_duration_secs)
+
+        # add remaining duration to current time
+        return calculation_time + travel_duration
 
     # provide angle diff in radians
     def distance(self, other, planet_angle_diff):
--- scientific.py
+++ scientific.py
@@ -1,5 +1,16 @@
+import math
+from datetime import timedelta
+
 light_speed = 299792458  # m/s
 
 
 def kin_duration(displacement, velocity):
     return displacement / velocity
+
+
+# angular velocity in rads/second
+def angular_velocity(period: timedelta):
+    return 2 * math.pi / period.total_seconds()
+
+
+print(angular_velocity(timedelta(days=100.5)))
